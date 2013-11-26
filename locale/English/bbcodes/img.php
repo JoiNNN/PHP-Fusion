afc77a0fe8ec699790ce29b043f17811e655f125
Dan C <spo0kye@yahoo.com>
2013-11-26 20:36:41 +0100

    Fixed IMG BBCode
    Fixed IMG BBCode RCE and CSRF vulnerability

--- locale/English/bbcodes/img.php
+++ locale/English/bbcodes/img.php
@@ -1,4 +1,7 @@
 <?php
 $locale['bb_img_description'] = "Displays image from given URL address";
 $locale['bb_img_usage'] = "Image URL";
+$locale['bb_img_error_not_valid'] = "This image is not valid";
+$locale['bb_img_error_fake'] = "This is not an image";
+$locale['bb_img_error_other'] = "There was an error";
 ?>
