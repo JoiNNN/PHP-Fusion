50107d96490daaedd1ee6c00b9eb4be2614c501e
Dan C <spo0kye@yahoo.com>
2013-12-30 18:07:36 +0100

    Update img_bbcode_include.php

--- includes/bbcodes/img_bbcode_include.php
+++ includes/bbcodes/img_bbcode_include.php
@@ -19,84 +19,14 @@
 if (!defined("IN_FUSION")) { die("Access Denied"); }
 
 if (!function_exists("img_bbcode_callback")) {
-   function img_bbcode_callback($matches) {
-
-   // Include locales
-   if (file_exists(LOCALE.LOCALESET."bbcodes/img.php")) {
-      include (LOCALE.LOCALESET."bbcodes/img.php");
-   } elseif (file_exists(LOCALE."English/bbcodes/img.php")) {
-      include (LOCALE."English/bbcodes/img.php");
-   }
-
-      if (substr($matches[3], -1, 1) != "/") {
-         $il = "$matches[1]$matches[3]$matches[4]";
-
-         if (function_exists('curl_init')) { // If cURL is available, use it.
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, $il);
-            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true); // This MUST be kept as it is used to block one of the XSRF image bypassing techniques.
-            curl_setopt($curl, CURLOPT_FILETIME, true);
-            curl_setopt($curl, CURLOPT_NOBODY, true);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_TIMEOUT, 2); // Execution time allowed in seconds, to prevent page hanging.
-            $header = curl_exec($curl);
-            $headers = explode("\n", $header);
-            $content_types = curl_getinfo($curl);
-            $content_size = curl_getinfo($curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
-            curl_close($curl);
-            $content_type = $content_types["content_type"];
-            $content_size = (int)$content_size;
-         } else {
-         	$content_type = get_headers($il,2);
-         	$content_type = $content_type['Content-Type'];
-            if (isset($content_type)) {
-               $chksize = get_headers($il,1);
-               $chksize = $chksize['Content-Length'];
-               $chksize = (int)$chksize;
-
-               if ($chksize >= 1 && $chksize != NULL && $chksize != false) {
-                  $content_type = get_headers($il,2);
-                  $content_type = $content_type['Content-Type'];
-                  $content_size = $chksize;
-                  $imageIsValid = true;
-               } else {
-                  $imageIsValid = false;
-                  $err_msg = $locale['bb_img_error_not_valid'];
-               }
-            } else {
-               $imageIsValid = false;
-               $err_msg = $locale['bb_img_error_not_valid'];
-            }
-         }
-
-         $content_type = $content_type; // This can still be faked, but as the FOLLOWLOCATION is used, they cannot fake YOUR header responses.
-
-         if (!preg_match("/image|jpg|jpeg|gif|png/i", $content_type)) {
-            $imageIsValid = false;
-            $err_msg = $locale['bb_img_error_not_valid'];
-         } else {
-            if (preg_match("/-/i", $content_size) || $content_size <= 2) {
-               $imageIsValid = false;
-               $err_msg = $locale['bb_img_error_fake'];
-            } else {
-               $imageIsValid = true;
-            }
-         }
-
-         // If image is valid
-         if ($imageIsValid == true) {   
-            return "<span style='display: block; max-width: 300px; max-height: 300px; overflow: auto;' class='forum-img-wrapper'><img src='".$matches[1].str_replace(array("?","&amp;","&","="), "", $matches[3]).$matches[4]."' alt='".$matches[3].$matches[4]."' style='border:0px' class='forum-img' /></span>";
-         // If image is invalid
-         } else {
-            return "<span class='invalid-image' style='text-decoration: line-through' title='".$err_msg."'>$matches[1]$matches[3]$matches[4]</span>";
-         }
-
-      // End of BBCode IMG tag patch by Euforia33
-      } else {
-         return "<span class='invalid-image' style='text-decoration: line-through' title='".$locale['bb_img_error_other']."'>$matches[1]$matches[3]$matches[4]</span>";
-      }
-   }
+        function img_bbcode_callback($matches) {
+                if (substr($matches[3], -1, 1) != "/") {
+                        return "<span style='display: block; width: 300px; max-height: 300px; overflow: auto;' class='forum-img-wrapper'><img src='".$matches[1].str_replace(array("?","&amp;","&","="), "", $matches[3]).$matches[4]."' alt='".$matches[3].$matches[4]."' style='border:0px' class='forum-img' /></span>";
+                } else {
+                        return $matches[0];
+                }
+        }
 }
 
-$text = preg_replace_callback("#\[img\]((http|ftp|https|ftps)://)(.*?)(\.(jpg|jpeg|gif|png|JPG|JPEG|GIF|PNG|PHP))\[/img\]#si", "img_bbcode_callback", $text);
+$text = preg_replace_callback("#\[img\]((http|ftp|https|ftps)://)(.*?)(\.(jpg|jpeg|gif|png|JPG|JPEG|GIF|PNG))\[/img\]#si", "img_bbcode_callback", $text);
 ?>
